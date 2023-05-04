9bfcce61f1d4642286e97287872864b177710960
Akintoye Adesomoju <aadesomoju001@gmail.com>
2023-05-03 21:20:56 -0400

    made a user interface using JS for the rock paper scissors game. only issue is there is a slight lag in showing score

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,17 @@
 // Create a function for the computer to make a choice
 let choices = ["Rock", "Paper", "Scissors"];
+let playerPoints = 0
+let computerPoints = 0
+
+function winnerPoints() {
+    if (playerPoints == 5 || computerPoints == 5) {
+        return false;
+    }
+    else {
+        return true;
+    }
+
+}
 
 function getComputerChoice() {
     let randChoice = Math.floor(Math.random() * choices.length + 0); // returning a random choice of rock paper or scissors
@@ -11,31 +23,56 @@ function round(playerSelection, computerSelection) {
     capsInitial = playerSelection[0].toUpperCase();
     playerSelection = playerSelection.toLowerCase();
     playerSelection = playerSelection.replace(playerSelection[0], capsInitial);
-    if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock"
-     || playerSelection == "Scissors" && computerSelection == "Paper") {
-        return `You Win! ${playerSelection} beats ${computerSelection}!`;
-     }
-    else if (playerSelection == "Rock" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Scissors"
-    || playerSelection == "Scissors" && computerSelection == "Rock") {
-        return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+    while (winnerPoints()) {
+        if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock"
+        || playerSelection == "Scissors" && computerSelection == "Paper") {
+            playerPoints = playerPoints + 1;
+            return `You Win! ${playerSelection} beats ${computerSelection}!`;
+        }
+        else if (playerSelection == "Rock" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Scissors"
+        || playerSelection == "Scissors" && computerSelection == "Rock") {
+            computerPoints = computerPoints + 1;
+            return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+        }
+        else if (playerSelection == "Rock" && computerSelection == "Rock" || playerSelection == "Paper" && computerSelection == "Paper"
+        || playerSelection == "Scissors" && computerSelection == "Scissors") {
+            return `It's a tie! ${playerSelection} ties ${computerSelection}!`;
+        }
+        else {
+            return "Invalid Choice";
+        }
     }
-    else if (playerSelection == "Rock" && computerSelection == "Rock" || playerSelection == "Paper" && computerSelection == "Paper"
-    || playerSelection == "Scissors" && computerSelection == "Scissors") {
-        return `It's a tie! ${playerSelection} ties ${computerSelection}!`;
+    if (playerPoints > computerPoints) {
+       return para.textContent = "Game over you Win!"
     }
     else {
-        return "Invalid Choice";
+       return para.textContent = "Game over you lose :("
     }
 }
 
-let roundsPlayed = 5;
 
 function game() {
-    for (let i = 0; i < roundsPlayed; i++ ) {
-        let playerSelection = prompt("Rock, Paper, or Scissors?");
-        let computerSelection = getComputerChoice();
-        console.log(round(playerSelection, computerSelection));
-    }
+    let playerSelection = this.textContent;
+    let computerSelection = getComputerChoice();
+    scoreboard.textContent = `Player: ${playerPoints} Computer: ${computerPoints}`;
+    para.textContent = `${round(playerSelection,computerSelection)}`;
+    
+
+
 }
 
-game();
+ // let roundsPlayed = 5;
+ let divvy = document.querySelector(".results")
+ let para = document.createElement("p");
+ para.textContent = "";
+ let scoreboard  = document.createElement("p");
+ scoreboard.textContent = " ";
+divvy.appendChild(scoreboard);
+divvy.appendChild(para);
+console.log(computerPoints)
+console.log(playerPoints)
+
+let buttons = document.querySelectorAll("button");
+buttons.forEach(button => button.addEventListener("click", game));
+
+
